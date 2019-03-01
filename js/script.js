918be8c3aaa25e4880b22aacbaf360714e03d023
Darian Newman <darian@cca.edu>
2019-02-28 21:21:42 -0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+$('document').ready(function(){
+  $('body').click(function(){
+    $('body').toggleClass('colorful');  
+  });
+  
+  	$(window).scroll(function() {
+		alert('Interactive 1/ Spring 2019');
+	});
+
+
+$('h1').hover(function() {
+  $('h7').fadeOut();
+}, function() {
+  $('h7').fadeIn();
+});
+
+
+
+
+
+});
+
+
