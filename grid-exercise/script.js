30d24656e0106db2808a804ce6373cc160ab1fe1
Darian Newman <darian@cca.edu>
2019-04-07 15:02:39 -0700

    Create script.js

---
+++ grid-exercise/script.js
@@ -0,0 +1,223 @@
+ $(document).ready(function(){
+
+      $('#item-1').mouseover(function(){
+        $('body').addClass('pink-bg');
+      });
+
+      $('#item-1').mouseout(function(){
+        $('body').removeClass('pink-bg');
+      });
+
+});
+
+$(document).ready(function(){
+
+      $('#item-2').mouseover(function(){
+        $('body').addClass('g-bg');
+      });
+
+      $('#item-2').mouseout(function(){
+        $('body').removeClass('g-bg');
+      });
+
+});
+
+$(document).ready(function(){
+
+      $('#item-3').mouseover(function(){
+        $('body').addClass('blue-bg');
+      });
+
+      $('#item-3').mouseout(function(){
+        $('body').removeClass('blue-bg');
+      });
+
+});
+
+$(document).ready(function(){
+
+      $('#item-4').mouseover(function(){
+        $('body').addClass('purple-bg');
+      });
+
+      $('#item-4').mouseout(function(){
+        $('body').removeClass('purple-bg');
+      });
+
+});
+
+$(document).ready(function(){
+
+      $('#item-5').mouseover(function(){
+        $('body').addClass('gr-bg');
+      });
+
+      $('#item-5').mouseout(function(){
+        $('body').removeClass('gr-bg');
+      });
+
+});
+
+$(document).ready(function(){
+
+      $('#item-6').mouseover(function(){
+        $('body').addClass('gra-bg');
+      });
+
+      $('#item-6').mouseout(function(){
+        $('body').removeClass('gra-bg');
+      });
+
+});
+
+$(document).ready(function(){
+
+      $('#item-7').mouseover(function(){
+        $('body').addClass('red-bg');
+      });
+
+      $('#item-7').mouseout(function(){
+        $('body').removeClass('red-bg');
+      });
+
+});
+
+$(document).ready(function(){
+
+      $('#item-8').mouseover(function(){
+        $('body').addClass('gray-bg');
+      });
+
+      $('#item-8').mouseout(function(){
+        $('body').removeClass('gray-bg');
+      });
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.playlist').hide();
+
+  } else {
+
+    $('.playlist').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.flower-1').hide();
+
+  } else {
+
+    $('.flower-1').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.flower-2').hide();
+
+  } else {
+
+    $('.flower-2').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.flower-3').hide();
+
+  } else {
+
+    $('.flower-3').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.flower-4').hide();
+
+  } else {
+
+    $('.flower-4').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.flower-5').hide();
+
+  } else {
+
+    $('.flower-5').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.flower-6').hide();
+
+  } else {
+
+    $('.flower-6').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.flower-7').hide();
+
+  } else {
+
+    $('.flower-7').show();
+
+    }
+
+});
+
+$(window).resize(function() {
+
+  if ($(this).width() < 800) {
+
+    $('.hide').hide();
+
+  } else {
+
+    $('.hide').show();
+
+    }
+
+});
+
+
